fixed incorrect index

ui.py
@@ -130,7 +130,7 @@ class ImageAudioConverter:
         d_width = image.shape[1] / ImageAudioConverter.SPLIT_NUMBER[1]
 
         ratio_h = np.floor(d_height) * ImageAudioConverter.SPLIT_NUMBER[0]
-        ratio_w = np.floor(d_width) * ImageAudioConverter.SPLIT_NUMBER[0]
+        ratio_w = np.floor(d_width) * ImageAudioConverter.SPLIT_NUMBER[1]
 
         image = cv2.resize(image, (int(ratio_h), int(ratio_w)))
 